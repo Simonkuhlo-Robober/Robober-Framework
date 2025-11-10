c3c02f41c34f6f51bdc1a42066917ef234ff5c32
Simon <siga241@gmail.com>
2025-11-10 12:21:14 +0100

    initial

---
+++ SimonsPluginResources/environment.py
@@ -0,0 +1,11 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .reelbot import ReelBot
+    from .settings.settings_manager import SettingsManager
+    from .logging.logger import Logger
+
+class Environment:
+    def __init__(self, bot: "ReelBot", settings: "SettingsManager", logger: "Logger"):
+        self.bot = bot
+        self.settings = settings
+        self.logger = logger
---
+++ SimonsPluginResources/logging/log_message_filters.py
@@ -0,0 +1,5 @@
+from SimonsPluginResources.Logger.log_message import LogMessage
+
+class LogMessageFilter:
+    def filter(message:LogMessage) -> bool:
+        return True
